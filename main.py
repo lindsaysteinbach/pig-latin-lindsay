b6897b47261605ab685d47ae5d3afa55e27b3609
lindsaysteinbach <lsteinbach@google.com>
2016-06-07 15:32:04 -0700

    added the templates folder and jinja

--- main.py
+++ main.py
@@ -15,6 +15,13 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+
+my_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -22,13 +29,13 @@ class MainHandler(webapp2.RequestHandler):
 
 class CountHandler(webapp2.RequestHandler):
     def get(self):
-        for number in range(1,10):
-            print "what happens"
-            self.response.write(number)
+        count_template=my_env.get_template('templates/count.html')
+        self.response.write(count_template.render())
 
 class PigHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('I can translate your phrases')
+        pig_template=my_env.get_template('templates/pig.html')
+        self.response.write(pig_template.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
